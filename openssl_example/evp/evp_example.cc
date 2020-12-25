f23c62d934fa7e2c30441d974944b9bfecd59e19
nani <renjie.0501@163.com>
2020-12-25 18:32:33 +0800

    add openssl evp example

--- openssl_example/evp/evp_example.cc
+++ openssl_example/evp/evp_example.cc
@@ -3,6 +3,7 @@
 #include <openssl/evp.h>
 #include <openssl/err.h>
 #include <openssl/rsa.h>
+#include <openssl/dh.h>
 
 void print_bin(unsigned char* tag ,unsigned char* data, int len)
 {
@@ -524,7 +525,7 @@ public:
         EVP_add_alg_module();
         ctx = EVP_PKEY_CTX_new(pkey, NULL);
         ret = EVP_PKEY_encrypt_init(ctx);
-        if(!ret)
+        if(ret != 1)
         {
             ERR_error_string(ERR_get_error(), err_string);
             printf("evp pkey encrypt init fail: %s\n", err_string);
@@ -570,7 +571,7 @@ public:
         EVP_add_alg_module();
         ctx = EVP_PKEY_CTX_new(pkey, NULL);
         ret = EVP_PKEY_decrypt_init(ctx);
-        if(!ret)
+        if(ret != 1)
         {
             ERR_error_string(ERR_get_error(), err_string);
             printf("evp pkey decrypt init fail: %s\n", err_string);
@@ -651,28 +652,448 @@ void pkey_rsa()
     EVP_PKEY_free(pub);
     EVP_PKEY_free(pri);
     OPENSSL_free(out);
+    OPENSSL_free(data);
     delete e;
     delete d;
+}
+
+void pkey_dh()
+{
+    char* in = "hello world";
+    size_t inlen = strlen(in);
+    unsigned char* out;
+    size_t outlen = 0;
+    unsigned char* data;
+    size_t datalen;
+    EVP_PKEY* pub;
+    EVP_PKEY* pri;
+    DH* dhPub= DH_new();
+    DH* dhPri= DH_new();
+    DH_generate_parameters_ex(dhPub, 512, DH_GENERATOR_2, NULL);
+    /* 生成公私钥 */
+    DH_generate_key(dhPub);
+    DH_set0_pqg(dhPri, BN_dup(DH_get0_p(dhPub)), NULL, BN_dup(DH_get0_g(dhPub)));
+    DH_generate_key(dhPri);
+
+    DH_set0_key(dhPub, BN_dup(DH_get0_pub_key(dhPri)), NULL); 
+    DH_set0_key(dhPri, BN_dup(DH_get0_pub_key(dhPub)), NULL);
+
+    pub = EVP_PKEY_new();
+    EVP_PKEY_set1_DH(pub, dhPub);
+
+    pri = EVP_PKEY_new();
+    EVP_PKEY_set1_DH(pri, dhPri);
+
+
+    evp_pkey_encrypt* e = new evp_pkey_encrypt(pub);
+    evp_pkey_decrypt* d = new evp_pkey_decrypt(pri);
+    e->evp_pkey_encrypt_info();
+    //=================================
+
+    e->encrypt((const unsigned char*)in, inlen, NULL, &outlen);
+    out = (unsigned char*)OPENSSL_zalloc(sizeof(unsigned char)*outlen);
+    e->encrypt((const unsigned char*)in, inlen, out, &outlen);
+    print_bin("enc data", out, outlen);
+
+    printf("------------------------------------------------------------\n");
+    d->evp_pkey_decrypt_info();
+    d->decrypt(out, outlen, NULL, &datalen);
+    data = (unsigned char*)OPENSSL_zalloc(sizeof(unsigned char)*datalen);
+    d->decrypt(out, outlen, data, &datalen);
+    printf("%d:%s\n", datalen, data);
+
+    DH_free(dhPub);
+    DH_free(dhPri);
+    EVP_PKEY_free(pub);
+    EVP_PKEY_free(pri);
+    OPENSSL_free(out);
+    OPENSSL_free(data);
 }
 
 void pkey()
 {
     pkey_rsa();
+    // 不支持DH
+    //pkey_dh();
 }
+
+
+void pbe()
+{
+    int ret;
+    char err_string[1024] = {0};
+    char* passwd = "123456";
+    int passwdlen = strlen(passwd);
+    int en = 1;
+    int de = 0;
+
+    char* in = "hello world";
+    int inlen = strlen(in);
+    unsigned char out[128] = {0};
+    int outlen = 128;
+
+    unsigned char d[128] = {0};
+    int dlen = 128;
+
+    char* algorithmname = "DES-CBC";
+    ERR_load_EVP_strings();
+    //=========================================================
+    
+    ASN1_OBJECT *algorithm;
+    const EVP_CIPHER *cipher;
+    ASN1_TYPE* parameter;
+    EVP_CIPHER_CTX* en_ctx;
+    EVP_CIPHER_CTX* de_ctx;
+    //========================加密过程========================================
+    parameter = ASN1_TYPE_new();
+    algorithm = OBJ_nid2obj(OBJ_sn2nid(algorithmname));
+    cipher = EVP_get_cipherbyobj(algorithm);
+
+    en_ctx = EVP_CIPHER_CTX_new();
+    EVP_CIPHER_CTX_set_padding(en_ctx, 0);
+    // 不设置key 和　iv
+    EVP_CipherInit_ex(en_ctx, cipher, NULL, NULL, NULL, en);
+    EVP_CIPHER_asn1_to_param(en_ctx, parameter);
+    //通过密码生成key 和　iv
+    EVP_PBE_CipherInit(algorithm, passwd, passwdlen, parameter,en_ctx, en);
+    int total = 0;
+    unsigned char* tmp = out;
+    ret = EVP_CipherUpdate(en_ctx, tmp, &outlen, in, inlen);
+    if(ret != 1)
+    {
+        ERR_error_string(ERR_get_error(), err_string);
+        printf("evp en cipher update fail: %s\n", err_string);
+    }
+    total += outlen;
+    tmp += outlen;
+    outlen = 128 - total;
+    ret = EVP_CipherFinal_ex(en_ctx, tmp, &outlen);
+    if(ret != 1)
+    {
+        ERR_error_string(ERR_get_error(), err_string);
+        printf("evp en cipher final fail: %s\n", err_string);
+    }
+    total += outlen;
+    print_bin("enc data", out, total);
+
+
+    ASN1_TYPE_free(parameter);
+    EVP_CIPHER_CTX_free(en_ctx);
+    //========================解密过程=======================
+
+    parameter = ASN1_TYPE_new();
+    algorithm = OBJ_nid2obj(OBJ_sn2nid(algorithmname));
+    cipher = EVP_get_cipherbyobj(algorithm);
+    de_ctx = EVP_CIPHER_CTX_new();
+    EVP_CIPHER_CTX_set_padding(de_ctx, 0);
+    // 不设置key 和　iv
+    EVP_CipherInit_ex(de_ctx, cipher, NULL, NULL, NULL, de);
+    EVP_CIPHER_asn1_to_param(de_ctx, parameter);
+
+    //通过密码生成key 和　iv
+    EVP_PBE_CipherInit(algorithm, passwd, passwdlen, parameter, de_ctx, de);
+
+    unsigned char* dtmp = d;
+    ret = EVP_CipherUpdate(en_ctx, dtmp, &dlen, out, total);
+    if(ret != 1)
+    {
+        ERR_error_string(ERR_get_error(), err_string);
+        printf("evp de cipher update fail: %s\n", err_string);
+    }
+    dtmp += dlen;
+    ret = EVP_CipherFinal_ex(en_ctx, dtmp, &dlen);
+    if(ret != 1)
+    {
+        ERR_error_string(ERR_get_error(), err_string);
+        printf("evp de cipher final fail: %s\n", err_string);
+    }
+    printf("%s\n", d);
+
+    ASN1_TYPE_free(parameter);
+    EVP_CIPHER_CTX_free(de_ctx);
+    EVP_cleanup();
+}
+
+
+class evp_sign
+{
+private:
+    const EVP_MD *md;
+    EVP_MD_CTX* md_sign_ctx;
+    EVP_PKEY *priKey;
+public:
+    evp_sign(EVP_PKEY* key): priKey(key)
+    {
+        md = EVP_md5();
+        md_sign_ctx = EVP_MD_CTX_new();
+        EVP_SignInit_ex(md_sign_ctx, md, NULL);
+    }
+
+    int evp_sign_update(char* data, int len)
+    {
+        return EVP_SignUpdate(md_sign_ctx, data, len);
+    }
+
+    int evp_sign_final(unsigned char *sig, unsigned int *s)
+    {
+        return EVP_SignFinal(md_sign_ctx, sig, s, priKey);
+    }
+
+    int evp_sign_max_length()
+    {
+        return EVP_PKEY_size(priKey);
+    }
+
+    ~evp_sign()
+    {
+        EVP_MD_CTX_free(md_sign_ctx);
+    }
+
+};
+
+class evp_verify
+{
+private:
+    const EVP_MD *md;
+    EVP_MD_CTX* md_verify_ctx;
+    EVP_PKEY *pubKey;
+public:
+    evp_verify(EVP_PKEY* key): pubKey(key)
+    {
+        md = EVP_md5();
+        md_verify_ctx = EVP_MD_CTX_new();
+        EVP_VerifyInit_ex(md_verify_ctx, md, NULL);
+    }
+
+    int evp_verify_update(char* data, int len)
+    {
+        return EVP_VerifyUpdate(md_verify_ctx, data, len);
+    }
+
+    int evp_verify_final(unsigned char *sig, unsigned int s)
+    {
+        return EVP_VerifyFinal(md_verify_ctx, sig, s, pubKey);
+    }
+
+    ~evp_verify()
+    {
+        EVP_MD_CTX_free(md_verify_ctx);
+    }
+
+};
+
+void sign_verify()
+{
+    BIGNUM* bne;
+    RSA* rsaParis;
+    RSA* rsaPublic;
+    RSA* rsaPrivate;
+    EVP_PKEY* pub;
+    EVP_PKEY* pri;
+    char* in = "hello world";
+    size_t inlen = strlen(in);
+
+    rsaParis = RSA_new();
+    bne = BN_new();
+    BN_set_word(bne, RSA_3);
+    RSA_generate_key_ex(rsaParis, 1024, bne, NULL);
+    rsaPublic = RSAPublicKey_dup(rsaParis);
+    rsaPrivate = RSAPrivateKey_dup(rsaParis);
+
+    pub = EVP_PKEY_new();
+    EVP_PKEY_set1_RSA(pub, rsaPublic);
+
+    pri = EVP_PKEY_new();
+    EVP_PKEY_set1_RSA(pri, rsaPrivate);
+//===========================================================================
+
+    evp_sign* s = new evp_sign(pri);
+    int maxlen = s->evp_sign_max_length();
+    unsigned char* signbuf = (unsigned char*)OPENSSL_zalloc(sizeof(unsigned char)*maxlen);
+    s->evp_sign_update(in, inlen);
+    unsigned int signlen = 0;
+    s->evp_sign_final(signbuf, &signlen);
+    print_bin("signature", signbuf, signlen);
+    printf("------------------------------------------------------------\n");
+
+    evp_verify* v = new evp_verify(pub);
+    //in = "world hello";
+    v->evp_verify_update(in, inlen);
+    int ret = v->evp_verify_final(signbuf, signlen);
+    if(ret == 1)
+    {
+        printf("signature verify success\n");
+    }
+    else
+    {
+        printf("signature verify fail\n");
+    }
+    
+
+
+    RSA_free(rsaParis);
+    RSA_free(rsaPublic);
+    RSA_free(rsaPrivate);
+    BN_free(bne);
+    EVP_PKEY_free(pub);
+    EVP_PKEY_free(pri);
+    OPENSSL_free(signbuf);
+    delete s;
+    delete v; 
+}
+
+//============================================================================
+// seal系列函数是相当于完成一个电子信封的功能，它产生一个随机密钥，
+// 然后使用一个公钥对该密钥进行封装，数据可以使用该随机密钥进行对称加密
+
+class evp_seal
+{
+private:
+    EVP_CIPHER_CTX *ctx;
+    const EVP_CIPHER *type;
+
+public:
+    evp_seal(const EVP_CIPHER *t,unsigned char **ek, int *ekl, unsigned char *iv, EVP_PKEY **pubk, int npubk):type(t)
+    {
+        ctx = EVP_CIPHER_CTX_new();
+        printf("xxxxxxx\n");
+        EVP_SealInit(ctx, type, ek, ekl, iv, pubk, npubk);
+    }
+
+    int evp_seal_update(unsigned char *in, int inl, unsigned char *out, int *outl)
+    {
+        return EVP_SealUpdate(ctx, out, outl, in, inl);
+    }
+
+    int evp_seal_final(unsigned char *out,int *outl)
+    {
+        return EVP_SealFinal(ctx, out, outl);
+    }
+
+    ~evp_seal()
+    {
+        EVP_CIPHER_CTX_free(ctx);
+    }
+};
+
+class evp_open
+{
+private:
+    EVP_CIPHER_CTX *ctx;
+    const EVP_CIPHER *type;
+public:
+    evp_open(const EVP_CIPHER *t,unsigned char *ek, int ekl, unsigned char *iv, EVP_PKEY* prik):type(t)
+    {
+        ctx = EVP_CIPHER_CTX_new();
+        EVP_OpenInit(ctx,type,ek,ekl,iv,prik);
+    }
+
+    int evp_open_update(unsigned char *in, int inl, unsigned char *out, int *outl)
+    {
+        return EVP_OpenUpdate(ctx, out, outl, in, inl);
+    }
+
+    int evp_seal_final(unsigned char *out,int *outl)
+    {
+        return EVP_OpenFinal(ctx, out, outl);
+    }
+
+    ~evp_open()
+    {
+        EVP_CIPHER_CTX_free(ctx);
+    }
+};
+
+void seal()
+{
+    BIGNUM* bne;
+    RSA* rsaParis;
+    RSA* rsaPublic;
+    RSA* rsaPrivate;
+    EVP_PKEY* pub;
+    EVP_PKEY* pri;
+    const EVP_CIPHER* cipher = EVP_des_cbc();
+    char* in = "hello world";
+    size_t inlen = strlen(in);
+
+    rsaParis = RSA_new();
+    bne = BN_new();
+    BN_set_word(bne, RSA_3);
+    RSA_generate_key_ex(rsaParis, 1024, bne, NULL);
+    rsaPublic = RSAPublicKey_dup(rsaParis);
+    rsaPrivate = RSAPrivateKey_dup(rsaParis);
+
+    pub = EVP_PKEY_new();
+    EVP_PKEY_set1_RSA(pub, rsaPublic);
+
+    pri = EVP_PKEY_new();
+    EVP_PKEY_set1_RSA(pri, rsaPrivate);
+   //===========================================================================
+   // 写信
+    int ekl = EVP_PKEY_size(pub);
+    char* ek = (char*) OPENSSL_zalloc(sizeof(char)*ekl);
+
+    int ekcnt = 1;
+    EVP_PKEY* pubarray[1];
+    pubarray[0] = pub;
+
+    char* ekarray[1];
+    ekarray[0] = ek;
+
+    evp_seal* s = new evp_seal(cipher, ekarray, &ekcnt, NULL, pubarray, 1);
+
+    printf("111111\n");
+    unsigned char out[1024] = {0};
+    int outlen = 1024;
+    unsigned char* tmp = out;
+    int total = 0;
+    s->evp_seal_update(in, inlen, tmp, &outlen);
+    tmp += outlen;
+    total+=outlen;
+    outlen = 1024- outlen;
+    s->evp_seal_final(tmp, &outlen);
+    total+=outlen;
+
+    //==========================================================
+    // 拆信
+    unsigned char d[1024] = {0};
+    int dlen = 1024;
+    unsigned char* dtmp = d;
+
+    evp_open *o = new evp_open(cipher,ek, ekl, NULL, pri);
+    o->evp_open_update(out, total, dtmp, &dlen);
+    dtmp += dlen;
+    o->evp_seal_final(dtmp, &dlen);
+
+    printf("%s\n", d);
+
+    OPENSSL_free(ek);
+    delete s;
+    delete o;
+}
+
+
 
 int main(int argc, char const *argv[])
 {
+    printf("====================================================\n");
     // 摘要
     //digest();
     printf("====================================================\n");
     // 对称解密
     //cipher();
-    printf("====================================================\n");
     // 对称加解密
     //crypto();
-    printf("====================================================\n");
     // 非对称加解密
-    pkey();
+    //pkey();
+    // 基于密码的加解密
+    // pbe();
+    printf("====================================================\n");
+    // 验签
+    //sign_verify();
+    printf("====================================================\n");
+    //　电子信封
+    seal();
 
     return 0;
 }
